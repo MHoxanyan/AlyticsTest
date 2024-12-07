6d0dacb9396786a907ee54f816c957181bf8a951
Sargis shirikchyan <shirikchyans@gmail.com>
2024-12-07 04:57:53 +0400

    configuration

--- docker/mysql/create-testing-database.sh
+++ docker/mysql/create-testing-database.sh
@@ -1,6 +1,2 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash  mysql --user=root --password="$MYSQL_ROOT_PASSWORD" <<-EOSQL     CREATE DATABASE IF NOT EXISTS testing;     GRANT ALL PRIVILEGES ON \`testing%\`.* TO '$MYSQL_USER'@'%'; EOSQL 
 
-mysql --user=root --password="$MYSQL_ROOT_PASSWORD" <<-EOSQL
-    CREATE DATABASE IF NOT EXISTS testing;
-    GRANT ALL PRIVILEGES ON \`testing%\`.* TO '$MYSQL_USER'@'%';
-EOSQL
